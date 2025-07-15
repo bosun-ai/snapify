b66d527e2cef71aaffe85f5c0741a0eabb73bc22
Daniel Hamandouche <daniel@numerico.tech>
2025-07-15 02:04:09 +0200

    Update datepicker on stats page

--- assets/forsure-charts-filters.js
+++ assets/forsure-charts-filters.js
@@ -34,7 +34,7 @@ function getProfiles(filters) {
 
 function getFilters() {
     getCountries(filterData);
-    getDates(filterData);
+    getDateOptions();
     getProfiles(filterData);
     // getReports(filterData);
     // getColumns(filterData);
@@ -48,21 +48,21 @@ function updateCountry() {
     filterData['country'] = selCountries;
     updateCharts();
     // Update the dates, reports, and columns based on selected country
-    getDates(filterData);
+    getDateOptions(selCountries);
     getProfiles(filterData);
     // getReports(filterFormData);
     // getColumns(filterFormData);
 }
 
-function updateDatepicker(start, end) {
-    filterData['start_time'] = start.format('YYYY-MM-DD');
-    filterData['end_time'] = end.format('YYYY-MM-DD');
-    updateCharts();
-    // Update the reports and columns based on selected date range
-    getProfiles(filterFormData);
-    // getReports(filterFormData);
-    // getColumns(filterFormData);
-}
+// function updateDatepicker(start, end) {
+//     filterData['start_time'] = start.format('YYYY-MM-DD');
+//     filterData['end_time'] = end.format('YYYY-MM-DD');
+//     updateCharts();
+//     // Update the reports and columns based on selected date range
+//     getProfiles(filterFormData);
+//     // getReports(filterFormData);
+//     // getColumns(filterFormData);
+// }
 function updateProfiles() {
     if (filterFormData.has('profile')) {
         filterFormData.delete('profile');
@@ -78,7 +78,7 @@ function updateProfiles() {
 }
 
 function updateCharts() {
-    createPlot();
+    createPlot(true);
     if (typeof redraw === 'function') {
         // check that redraw is known
         redraw();
@@ -174,74 +174,74 @@ async function initDropdowns() {
     }
 }
 
-async function getDates() {
-    try {
-        const response = await unifiedSendRequest(STATSREPORTS, {formData: JSON.stringify(filters), 
-            headerArgs: {'Content-Type': 'application/json'}});
-        // Find min start date and max end date
-        const minDate = new Date(response.start_time);
-        const maxDate = new Date(response.end_time);
-        const startDate = new Date(maxDate);
-        startDate.setDate(startDate.getDate() - 30);
-        function tsToDate(ts) {
-            var d = new Date(ts);
-            var lang = "en-NL";
-            return d.toLocaleDateString(lang, {
-                year: 'numeric',
-                month: 'long',
-                day: 'numeric'
-            });
-        }
+// async function getDates() {
+//     try {
+//         const response = await unifiedSendRequest(STATSREPORTS, {formData: JSON.stringify(filters), 
+//             headerArgs: {'Content-Type': 'application/json'}});
+//         // Find min start date and max end date
+//         const minDate = new Date(response.start_time);
+//         const maxDate = new Date(response.end_time);
+//         const startDate = new Date(maxDate);
+//         startDate.setDate(startDate.getDate() - 30);
+//         function tsToDate(ts) {
+//             var d = new Date(ts);
+//             var lang = "en-NL";
+//             return d.toLocaleDateString(lang, {
+//                 year: 'numeric',
+//                 month: 'long',
+//                 day: 'numeric'
+//             });
+//         }
 
 
-        $('#monthPicker').daterangepicker({
-            opens: 'center',
-            showDropdowns: true,
-            linkedCalendars: false,
+//         $('#monthPicker').daterangepicker({
+//             opens: 'center',
+//             showDropdowns: true,
+//             linkedCalendars: false,
 
-            // autoApply: true,
-            startDate: startDate,
-            endDate: maxDate,
-            minDate: minDate,
-            maxDate: maxDate,
-            locale: {
-                format: 'DD-MM-YYYY'
-            },
-            ranges: {
-                'This Month': [
-                    moment().startOf('month'), moment().endOf('month')
-                ],
-                'Last 3 Months': [
-                    moment().subtract(2, 'month').startOf('month'),
-                    moment().endOf('month')
-                ],
-                'Last 6 Months': [
-                    moment().subtract(5, 'month').startOf('month'),
-                    moment().endOf('month')
-                ],
-                'Last Year': [
-                    moment().subtract(1, 'year').startOf('year'),
-                    moment().subtract(1, 'year').endOf('year')
-                ],
-                'Last 2 Years': [
-                    moment().subtract(2, 'years').startOf('year'),
-                    moment().subtract(1, 'year').endOf('year')
-                ],
-                'Last 5 Years': [
-                    moment().subtract(5, 'years').startOf('year'),
-                    moment().subtract(1, 'year').endOf('year')
-                ]
-            }
-        }, function (start, end, label) {
-            updateDatepicker(start, end);
-            if (typeof redraw === 'function') {
-                // check that redraw is known
-                redraw();
-            }
-        });
+//             // autoApply: true,
+//             startDate: startDate,
+//             endDate: maxDate,
+//             minDate: minDate,
+//             maxDate: maxDate,
+//             locale: {
+//                 format: 'DD-MM-YYYY'
+//             },
+//             ranges: {
+//                 'This Month': [
+//                     moment().startOf('month'), moment().endOf('month')
+//                 ],
+//                 'Last 3 Months': [
+//                     moment().subtract(2, 'month').startOf('month'),
+//                     moment().endOf('month')
+//                 ],
+//                 'Last 6 Months': [
+//                     moment().subtract(5, 'month').startOf('month'),
+//                     moment().endOf('month')
+//                 ],
+//                 'Last Year': [
+//                     moment().subtract(1, 'year').startOf('year'),
+//                     moment().subtract(1, 'year').endOf('year')
+//                 ],
+//                 'Last 2 Years': [
+//                     moment().subtract(2, 'years').startOf('year'),
+//                     moment().subtract(1, 'year').endOf('year')
+//                 ],
+//                 'Last 5 Years': [
+//                     moment().subtract(5, 'years').startOf('year'),
+//                     moment().subtract(1, 'year').endOf('year')
+//                 ]
+//             }
+//         }, function (start, end, label) {
+//             updateDatepicker(start, end);
+//             if (typeof redraw === 'function') {
+//                 // check that redraw is known
+//                 redraw();
+//             }
+//         });
 
-    } catch (error) {
-        console.error(error.message);
-        TOKEN = false;
-    }
-}
+//     } catch (error) {
+//         console.error(error.message);
+//         TOKEN = false;
+//     }
+// }
